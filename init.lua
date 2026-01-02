cb5277727a55e1a2ea7d620f308858950f1b0099
deverivaldofuentes-arch <dev.erivaldofuentes@gmail.com>
2026-01-01 20:42:39 -0400

    Nuevo inicio

--- init.lua
+++ init.lua
@@ -1,13 +1,37 @@
--- Neovim 0.11+ Configuration
--- ==========================
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
 
--- Silenciar advertencias
-vim.g.lspconfig_silent = true
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- 1. Cargar configuración base
-require('core.options')
-require('core.keymaps')
-require('core.autocmds')
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
 
--- 2. Cargar gestor de plugins y configuraciones
-require('plugins.setup')
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
